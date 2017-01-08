69027eaa3c66626b3358bf1b9b3da17c4628ca64
rafel <rafelisraels@gmail.com>
2017-01-08 16:27:57 +0100

    v8 make -j5

--- non_apt_install.sh
+++ non_apt_install.sh
@@ -23,7 +23,7 @@ unzip opencv-3.0.0-alpha.zip
 mkdir opencv-3.0.0-alpha/build
 cd opencv-3.0.0-alpha/build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=~/local ..
-make -j4
+make -j5
 make install
 
 # -3- OPENBABEL
@@ -37,7 +37,7 @@ mv qqq ./openbabel-2.3.2/include/openbabel/shared_ptr.h
 mkdir build
 cd build
 cmake ../openbabel-2.3.2 -DCMAKE_INSTALL_PREFIX=../../  # prefix=~/local fails in ubuntu 16.04: must be relativ
-make -j4
+make -j5
 make install
 cd ~/local/include
 ln -s openbabel-2.0/openbabel
@@ -73,11 +73,12 @@ git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=`pwd`/depot_tools:"$PATH"
 fetch v8
 cd v8
-make x64.release # takes ~ 1 hour
+make -j5 x64.release # takes ~ 1 hour
 mkdir -p ~/local/include/v8
 cp -r include/* ~/local/include/v8
 cp out/x64.release/obj.target/src/lib*.a ~/local/lib
 cp out/x64.release/obj.target/third_party/icu/lib*.a ~/local/lib
+cp out/x64.release/*.bin ~/local/lib
 
 
 

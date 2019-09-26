c4d6e08f0b437f15f244a71d229643533ceb3999
rafel <rafelisraels@gmail.com>
2019-09-26 16:59:14 +0200

    three no longer linked from build, xmlhttprequest no longer installed

--- npmInstall.sh
+++ npmInstall.sh
@@ -8,15 +8,15 @@ npm install eslint                    # parsing js-code
 npm install jsdoc             # documentation of js-projects
 
 
- #npm install sha3                      # see: https://codahale.com/how-to-safely-store-a-password/
- npm install bcrypt                      # extServer
+ #npm install sha3                # see: https://codahale.com/how-to-safely-store-a-password/
+ npm install bcryptjs             # extServer
  npm install b64                  # base64 as streamfilter ?
  npm install string-hash          # used in momonotes/latex
  npm install utf8                 # used in momonotes/latex
- npm install xmlhttprequest       # usdin in momonotes/ajax
+# no longer needed ???? (20190926) ??? npm install xmlhttprequest       # usdin in momonotes/ajax
  npm install three                # WEB-GL wrapper, used in molCompare.js
-cd ~/local/lib/node_modules/three
-ln -s build/three.min.js              # momonotes expects to find three.min.js in three/
+# 20190926 cd ~/local/lib/node_modules/three
+# ln -s build/three.min.js          # momonotes expects to find three.min.js in three/
 
 
 
